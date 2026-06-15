0f01a3283eabe5e2041c9c02c77e87987f3be4bf
rafimhdy <rafimhdy@gmail.com>
2026-06-15 14:43:45 +0700

    fix: use relative base path and clean build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // Tambahkan baris ini (sesuaikan dengan nama repository kamu):
-  base: "/website-translate/",
+  base: "./",
 });
